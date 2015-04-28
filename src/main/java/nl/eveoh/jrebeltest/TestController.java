d8a08f0a75dd0a4c42ae474bf5e9285994ef8a47
Mike Noordermeer <mike@eveoh.nl>
2015-04-28 10:36:51 +0200

    Initial commit.

---
+++ src/main/java/nl/eveoh/jrebeltest/TestController.java
@@ -0,0 +1,37 @@
+package nl.eveoh.jrebeltest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${testProperty2}")
+    private String testValue;
+
+    @RequestMapping(value = "/test1")
+    @ResponseBody
+    public String testMethod1(){
+        return "Test1";
+    }
+
+    @RequestMapping(value = "/test2")
+    @ResponseBody
+    public String testMethod2(){
+        return environment.getProperty("testProperty1");
+    }
+
+    @RequestMapping(value = "/test3")
+    @ResponseBody
+    public String testMethod3(){
+        return testValue;
+    }
+
+}
